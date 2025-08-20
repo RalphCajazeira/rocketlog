7e5cadf3e23241a0ceba364d825dbc7634a0cd11
Ralph Ramos <ralphmtk@gmail.com>
2025-08-20 17:04:58 -0300

    Retornar Token do usuário

---
+++ src/controllers/sessions-controllers.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express";
+import { AppError } from "@/utils/AppError";
+import { prisma } from "@/database/prisma";
+import { compare } from "bcrypt";
+import { z } from "zod";
+
+class SessionsController {
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      email: z.string().email(),
+      password: z.string().min(6),
+    });
+
+    const { email, password } = bodySchema.parse(request.body);
+
+    const user = await prisma.user.findFirst({ where: { email } });
+
+    if (!user) {
+      throw new AppError("Invalid email or password", 401);
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new AppError("Invalid email or password", 401);
+    }
+
+    return response.json({ message: "Ok" });
+  }
+}
+
+export { SessionsController };
