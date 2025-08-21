f9dcec60dbc6d33c58d13ba0d315ba39635a7598
Ralph Ramos <ralphmtk@gmail.com>
2025-08-21 16:59:39 -0300

    Alteração de status do delivery

--- src/controllers/deliveries-controller.ts
+++ src/controllers/deliveries-controller.ts
@@ -1,8 +1,44 @@
+import { prisma } from "@/database/prisma";
 import { Request, Response } from "express";
+import z from "zod";
 
 class DeliveriesController {
-  create(request: Request, response: Response) {
-    return response.json({ message: "ok" });
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      user_id: z.string().uuid(),
+      description: z.string().trim().min(2),
+    });
+
+    const { user_id, description } = bodySchema.parse(request.body);
+
+    await prisma.delivery.create({
+      data: {
+        userId: user_id,
+        description,
+      },
+    });
+
+    return response.status(201).json();
+  }
+
+  async index(request: Request, response: Response) {
+    const deliveries = await prisma.delivery.findMany({
+      include: {
+        user: { select: { name: true, email: true } },
+      },
+    });
+
+    return response.json(deliveries);
+  }
+
+  async show(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const delivery = await prisma.delivery.findUnique({
+      where: { id },
+    });
+
+    return response.json(delivery);
   }
 }
 
